4835c32291df3f93565331a79e94785882b32431
Christian Clausen <csbc92@hotmail.com>
2019-09-27 15:58:44 +0200

    Removed unused import

--- dk.grp1.tanks.common/src/test/dk/grp1/tanks/common/data/GameMapTest.java
+++ dk.grp1.tanks.common/src/test/dk/grp1/tanks/common/data/GameMapTest.java
@@ -1,6 +1,5 @@
 package dk.grp1.tanks.common.data;
 
-import com.sun.javafx.geom.Vec2d;
 import dk.grp1.tanks.common.utils.GameMapFunctionComparator;
 import dk.grp1.tanks.common.utils.Vector2D;
 import org.junit.Before;
